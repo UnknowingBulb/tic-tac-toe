4a6edc89004652fee85d678dd2d8966d8a62d1cb
UnknowingBulb <aldoshina.an@gmail.com>
2022-07-14 20:50:36 +1000

    replaced NavBar with Footer

---
+++ XOX/ClientApp/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class Footer extends Component {
+
+  render () {
+    return (
+        <footer>
+            <div>Site use cookies. Leave if you don't agree with this.</div>
+            {(window.location.href.includes('localhost')) === true ? <div>github repo <a href="https://github.com/UnknowingBulb/tic-tac-toe">tic-tac-toe</a> </div> : null }
+        </footer>
+    );
+  }
+}
